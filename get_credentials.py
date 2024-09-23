c5f46bbf421cacb603cb1854257177f1df706005
Jason Kristanto <96901367+jasonkristanto188@users.noreply.github.com>
2024-09-23 19:54:37 +0700

    Update get_credentials.py

--- get_credentials.py
+++ get_credentials.py
@@ -1,13 +1,23 @@
-#do NOT encrypt this file
+# clone this file and use the cloned file for encryption
+#
+# the logic behind this code is to read an encrypted excel file that contains credentials
+# the excel file is a 2x2 dataframe, with its two columns named 'Username' and 'Password'
 
 import io
 import msoffcrypto
 import pandas as pd
 
 def get_data():
-    with open(r"D:\Users\jason.kristanto\Desktop\Excel Password.xlsx", 'rb') as file:
+    # this excel path contains your credentials
+    excelpath = r"...\credentials.xlsx"
+    
+    with open(excelpath, 'rb') as file:
         office_file = msoffcrypto.OfficeFile(file)
-        office_file.load_key('Lalabora29')
+
+        # the excel password
+        excelpassword = '...'
+        
+        office_file.load_key(excelpassword)
         decrypted_workbook = io.BytesIO()
         office_file.decrypt(decrypted_workbook)
         df = pd.read_excel(decrypted_workbook)  
